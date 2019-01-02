d990833ec7bb069272e841396b79422f1094de10
Lordaloa <reinout_van@hotmail.com>
2019-01-02 18:02:11 +0100

    full restore from todays work

--- src/main/java/be/ucll/da/Application.java
+++ src/main/java/be/ucll/da/Application.java
@@ -17,13 +17,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Date;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
@@ -32,11 +30,30 @@ public class Application {
     }
 
     @Configuration
+    @EnableWebMvc
     public class WebConfig implements WebMvcConfigurer{
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**").allowedOrigins("http://localhost:4200","http://localhost:8080").allowedMethods("GET","PUT","POST","DELETE","OPTIONS");
         }
+    }
+
+    @Bean
+    public CommandLineRunner SandwichesCommandLineRunner(SandwichRepository sandwichRepository){
+        return (args) -> {
+            sandwichRepository.save(new Sandwich("Martino",new BigDecimal(2.50),"Martino,Ansjovis,Augurk,Ajuin,PikanteSaus"));
+            sandwichRepository.save(new Sandwich("Smos",new BigDecimal(2.50),"Kaas,Hesp,Sla,Tomaat,Eitjes,Worteltjes,Mayonaiss"));
+            sandwichRepository.save(new Sandwich("Kaas",new BigDecimal(2.50),"Kaas,Sla,Tomaat,Eitjes,Worteltjes,Mayonaiss"));
+            sandwichRepository.save(new Sandwich("Hesp",new BigDecimal(2.50),"Hesp,Sla,Tomaat,Eitjes,Worteltjes,Mayonaiss"));
+            sandwichRepository.save(new Sandwich("Brie",new BigDecimal(2.50),"Brie"));
+        };
+    }
+
+    @Bean
+    public CommandLineRunner OrderCommandLineRunner(OrderRepository orderRepository){
+        return (args) -> {
+//            orderRepository.save(new Order(new Sandwich("Martino",new BigDecimal(2.50),"Martino,Ansjovis,Augurk,Ajuin,PikanteSaus"), LocalDate.of(1996,7,13), BreadType.WRAP,"046848695"));
+        };
     }
 
     @Bean
